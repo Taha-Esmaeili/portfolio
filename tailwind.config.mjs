7ae562e02d4ae29e9c3b7ee97b3949cba3cb491c
Taha Esmaeili <taha.karbalaee@gmail.com>
2026-08-28 17:21:16 +0330

    Fix Dark Mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,7 @@ export default {
   content: [
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
   ],
-  darkMode: 'class',
+  darkMode: ['selector', '[data-theme="dark"]'],
   theme: {
     extend: {
       colors: {
